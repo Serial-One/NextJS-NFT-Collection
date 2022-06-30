81756dd344689751619166060ec6a5bb1f785bd5
Rich Tillman <richtillman@gmail.com>
2022-06-30 13:56:50 -0700

    feat: use our own API endpoint

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = '0x911F158D30C948C1591054dCbC9f74115cF827FC'
 // URL for the Crypto Devs NFT Metadata
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/'
+const METADATA_URL =
+  'https://62be0c48093a22177b652ea5--roaring-sfogliatella-9b44cd.netlify.app/api/'
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
