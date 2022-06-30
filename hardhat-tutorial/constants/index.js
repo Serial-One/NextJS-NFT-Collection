10fd1dae01115d23c2ceaac9041c75b0a8ca3ddc
Rich Tillman <richtillman@gmail.com>
2022-06-30 11:33:39 -0700

    feat: first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = '0xaeC59D77A0045bFCaC73DbE9944e9d98145AE9D4'
+// URL for the Crypto Devs NFT Metadata
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
